04cc4cc2bb37c33d4e3b7eaab95286a340ef6d1d
Shawn <78675587+shawnlit@users.noreply.github.com>
2026-01-05 21:24:57 +0530

    changed serial print format to csv format

--- ARK/system/sensors.cpp
+++ ARK/system/sensors.cpp
@@ -139,8 +139,14 @@ void SensorManager::Update() {
 void SensorManager::BuildPacket() {
     char buf[512];
     uint32_t ts = to_ms_since_boot(get_absolute_time());
-    snprintf(buf, sizeof(buf), 
-             "T:%lu | B:%d M:%d I:%d S:%d | A:%.1f | H:%.1f | ACC:%.2f,%.2f,%.2f | GYR:%.1f,%.1f,%.1f", 
-             ts, _b_ok, _m_ok, _imu_ok, _s_ok, _alt, _hdg, _ax, _ay, _az, _gx, _gy, _gz);
+    //snprintf(buf, sizeof(buf), 
+    //         "T:%lu | B:%d M:%d I:%d S:%d | A:%.1f | H:%.1f | ACC:%.2f,%.2f,%.2f | GYR:%.1f,%.1f,%.1f", 
+    //         ts, _b_ok, _m_ok, _imu_ok, _s_ok, _alt, _hdg, _ax, _ay, _az, _gx, _gy, _gz);
+
+    
+    snprintf(buf, sizeof(buf),
+            "%lu,%d,%d,%d,%d,%.1f,%.1f,%.2f,%.2f,%.2f,%.1f,%.1f,%.1f",
+            ts, _b_ok, _m_ok, _imu_ok, _s_ok, _alt, _hdg, _ax, _ay, _az, _gx, _gy, _gz);
+    
     _packet = std::string(buf);
 }
